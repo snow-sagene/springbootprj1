d7aec1221848cdcf143359781f30bfabe4b85db9
FLEKKE\vtr04 <0429lyb@naver.com>
2023-11-26 16:57:09 +0900

    post Mapper 업데이트 딜리트 테스트 완료

--- Board/src/test/java/com/study/PostMapperTest.java
+++ Board/src/test/java/com/study/PostMapperTest.java
@@ -48,8 +48,39 @@ public class PostMapperTest {
         //3. 이후 행동
         System.out.print(res);
 
-        System.out.println("영빈");
     }
 
+
+    @Test
+    void updatePost(){
+        //1.테스트 준비
+        PostRequest params= new PostRequest();
+        params.setId(1l);
+        params.setTitle("1번 게시글의 제목을 수정");
+        params.setContent("1번 게시글의 내용을 수정");
+        params.setWriter("홍길동");
+        params.setNoticeYn(false);
+        PostResponse res1 = postMapper.findById(params.getId());
+        System.out.println("바꾸기 전의 1번글: "+res1);
+
+
+        //2.테스트실행
+        postMapper.update(params);
+
+
+
+        //3.테스트 이후
+        //글 수정이 잘 되엇는지 체크해야하니까 해당글번호로 다시 글 조회해보기
+        PostResponse res2 = postMapper.findById(params.getId());
+        System.out.println("바꾼후의 1번글: "+res2);
+
+    }
+
+      @Test
+    void delete() {
+        System.out.println("삭제 이전의 전체 게시글 개수는 : " + postMapper.findAll().size() + "개입니다.");
+        postMapper.deleteById(1L);
+        System.out.println("삭제 이후의 전체 게시글 개수는 : " + postMapper.findAll().size() + "개입니다.");
+    }
 
 }
