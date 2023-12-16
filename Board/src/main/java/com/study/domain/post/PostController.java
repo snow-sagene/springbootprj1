df51560e4550320a960e786ccd9895fa30c49cc2
FLEKKE\vtr04 <vtr0405@naver.com>
2023-12-16 15:52:53 +0900

    글쓰기 작성(수정), db 넘어가는것 까지 확인 완료

--- Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/src/main/java/com/study/domain/post/PostController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,18 +13,26 @@ public class PostController {
 
     private final PostService postService;
 
-    // 게시글 작성 페이지
+    // 게시글 작성-수정 페이지 dlehd
     @GetMapping("/post/write.do")
-    public String openPostWrite(Model model){
-        String title = "자바에서 만든 제목",
-                content = "자바에서 만든 내용",
-                write = "홍길동";
-
-        model.addAttribute("t", title);
-        model.addAttribute("c", title);
-        model.addAttribute("w", write);
+    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model){
+        //글 번홓가 있다면 해당 글번호의 정보를 담아서 글쓰기 화면 리턴j
+        if (id != null) { //수정
+            postService.findPostById(id); //write.do 함수 하나로 글쓰기와 글수정을 같이 하기 때문에
+            PostResponse post = postService.findPostById(id);
+            //해당 글 수정했을 때는 수정한 글 번호가 반환되고, 새글 작성시 null이 반환됨
+            model.addAttribute("post", post);
+        }
+        // 글번호가 없다면 비어있는 글쓰기 화면 리턴(새글쓰기)
         return "post/write";
 
     }
 
-}
+    @PostMapping("/post/save.do")
+    public String savePost(final PostRequest params) {
+        postService.savePost(params);
+        return "post/write";
+
+
+    }
+};
